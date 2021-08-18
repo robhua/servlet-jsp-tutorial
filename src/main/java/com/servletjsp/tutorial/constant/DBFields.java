0421714bb12f275b11e239fda3f9c652a2bf241e
Hung Hua <hunghm.ce@gmail.com>
2021-08-18 20:21:51 +0700

    Delete the unuse code.

--- src/main/java/com/servletjsp/tutorial/constant/DBFields.java
+++ src/main/java/com/servletjsp/tutorial/constant/DBFields.java
@@ -4,11 +4,6 @@ public class DBFields {
     public static class PRODUCT {
         public final static String PRODUCT_ID = "PRODUCT.PRODUCT_ID";
         public final static String PRODUCT_NAME = "PRODUCT.PRODUCT_NAME";
-//        `IMAGE` Longtext NULL,
-//        `PRICE` Decimal(15,4) NULL,
-//        `TITLE` Longtext NULL,
-//        `DESC` Longtext NULL,
         public final static String CATEGORY_ID = "PRODUCT.CATEGORY_ID";
-//        `SELL_ID` int NULL
     }
 }
